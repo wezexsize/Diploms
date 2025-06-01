735385e6dfa0227acc33be1a542a56fbe8574ccb
wezexsize <ilapavliv79@gmail.com>
2025-06-01 13:49:02 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+package com.webstore;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String jdbcURL = "jdbc:mysql://localhost:3306/webstore";
+        String dbUser = "root";
+        String dbPassword = "password";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcURL, dbUser, dbPassword);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT id, name, price FROM products");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                double price = resultSet.getDouble("price");
+
+                System.out.println("ID: " + id + ", Название: " + name + ", Цена: " + price);
+            }
+
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
